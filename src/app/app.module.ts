703a5bf3d5ed537551fdca96937a2872e5e7703b
Adam-Szitas <example.eu.sk.number@gmail.com>
2021-08-09 14:13:42 +0200

    creating colors scss but some issues happened

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    AuthenticationComponent
   ],
   imports: [
     BrowserModule,
